4f4941a0391b908d42a1e10d7eb5fe9e4d5f1a89
aldoprogrammer <aldobesma@gmail.com>
2024-08-18 14:49:46 +0700

    first commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.9;
 
 contract MyContract {
+    struct Coffee {
+        address sender;
+        string message;
+        uint256 timestamp;
+    }
+
+    uint256 totalCoffe;
+    address payable owner;
+
+    event NewCoffee(address indexed sender, string message, uint256 timestamp);
     constructor() {}
 }
