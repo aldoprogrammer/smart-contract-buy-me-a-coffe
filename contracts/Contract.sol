e44c17b7a8427f3be21276ede061b14efb171275
aldoprogrammer <aldobesma@gmail.com>
2024-08-18 15:27:10 +0700

    done

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -12,5 +12,25 @@ contract MyContract {
     address payable owner;
 
     event NewCoffee(address indexed sender, string message, uint256 timestamp);
-    constructor() {}
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyMeACoffee(
+        string memory _message
+        ) public payable {
+          require(msg.value > 0, "value must be greater than zero");
+
+          totalCoffe += 1;
+
+          payable(owner).transfer(msg.value);
+          emit NewCoffee(msg.sender, _message, block.timestamp);
+    }
+
+    function getTotalCoffe() public view returns (uint256) {
+        return totalCoffe;
+    }
+
+    
 }
